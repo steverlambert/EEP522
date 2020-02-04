cadebd57d2f6633cb7b716ea09576c711c1a29f0
steverlambert <steverlambert@gmail.com>
2020-02-04 14:30:59 -0800

    added camera tests

--- Assignment2/gpio_tests.c
+++ Assignment2/gpio_tests.c
@@ -1,6 +1,11 @@
-# include <wiringPi.h>
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdio.h>
 
 void run_led(int pin);
+struct timeval t1, t2;
+
 
 int main (void) {
 	wiringPiSetup();
@@ -13,6 +18,7 @@ int main (void) {
 			run_led(0);
 			delay(500);
 			digitalWrite(0, LOW);
+			break;
 		}
 	}
 	return 0;
@@ -20,4 +26,11 @@ int main (void) {
 
 void run_led(int pin) {
 	digitalWrite(pin, HIGH);
+	gettimeofday(&t1, NULL);
+	unsigned long init = 1000000 * t1.tv_sec + t1.tv_usec;
+	system("raspistill -o testimg2.jpg -t 1 -h 64 -w 64 -n");
+	gettimeofday(&t2, NULL);
+	unsigned long aft = 1000000 * t2.tv_sec + t2.tv_usec;
+	printf("Time to take picture: %lu ms\n", aft-init);
 }
+
